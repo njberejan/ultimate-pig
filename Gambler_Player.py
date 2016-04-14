a2203cba7c44f3703eb4ff8659b6e0d14e7746ae
Nick Berejan <njberejan@gmail.com>
2016-04-13 22:15:38 -0400

    Delete Gambler_Player.py

--- Gambler_Player.py
+++
@@ -1,6 +0,0 @@
-#Gamebler will play every turn for seven turns and never bow out before end of round 7. Boom or bust.
-from Player_Class import Player
-class GamblerPlayer(Player):
-    #__innit__ IS NOT NEEDED BECAUSE ATTRIBUTES DEFINED IN PARENT CLASS WON'T CHANGE FOR CHILD CLASSES
-    def roll_again(self):
-        return True
